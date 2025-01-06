2cd676c2fe5c7722dbc615d068b15030cee65301
Bloodhaven Studios <106410531+BloodhavenStudios@users.noreply.github.com>
2025-01-06 14:44:29 -0600

    tidy up code

--- load_assets.js
+++ load_assets.js
@@ -6,3 +6,12 @@ fetch("/QuartzEngine/assets/topnav")
 	.catch(error => {
 		console.error("Error loading navbar:", error);
 	});
+
+fetch("/QuartzEngine/assets/footer")
+	.then(response => response.text())
+	.then(html => {
+		document.getElementById("footer").innerHTML = html;
+	})
+	.catch(error => {
+		console.error("Error loading footer:", error);
+	});
